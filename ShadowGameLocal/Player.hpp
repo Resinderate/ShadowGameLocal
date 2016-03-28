d46c9f283f726299c192c4953d5c7f96cd184195
Ronan Murphy <ronan_murfy@hotmail.com>
2016-03-28 16:24:46 +0100

    Revert "Attack with new sprite, animation bug"
    This reverts commit e9ee1d6a4a7f2104c9135f339b82ef1a2307649f.

--- ShadowGameLocal/Player.hpp
+++ ShadowGameLocal/Player.hpp
@@ -3,32 +3,33 @@
 #include "SFML\Graphics.hpp"
 #include "ShadowFactory.hpp"
 #include <vector>
+#include "AnimatedSprite.hpp"
+#include "Animation.hpp"
 //Kevin
-class Player : public sf::Sprite
+class Player : public AnimatedSprite
 {
 public:
 	Player();
-	Player(std::string p_username, std::string p_password, sf::Vector2f p_position, sf::Vector2f p_scale, uint8_t keySet);
-	
+	Player(std::string p_username, std::string p_password, sf::Vector2f p_position, uint8_t keySet, std::vector<Animation> p_animations);
+	inline std::string& GetUsername() { return m_username; }
+	inline std::string& GetPassword() { return m_password; }
+	void Update(ShadowFactory p_shadowFactory, float p_delta);
+	void Move(sf::Vector2f p_direction, ShadowFactory p_shadowFactory);
+	void InputHandler(ShadowFactory p_shadowFactory, float p_delta);
+
 	//void Update(ShadowFactory p_shadowFactory, std::vector<Player> p_players);
 	//void Move(sf::Vector2f p_direction, ShadowFactory p_shadowFactory);
 	//void InputHandler(ShadowFactory p_shadowFactory, std::vector<Player> p_players);
-	void Attack(std::vector<Player> p_players);
-	void Attack2(std::vector<Player> p_players);
-
-	inline std::string& GetUsername() { return m_username; }
-	inline std::string& GetPassword() { return m_password; }
-	void Update(ShadowFactory p_shadowFactory, float p_delta, std::vector<Player> p_players);
-	void Move(sf::Vector2f p_direction, ShadowFactory p_shadowFactory);
-	void InputHandler(ShadowFactory p_shadowFactory, float p_delta, std::vector<Player> p_players);
+	//void Attack(std::vector<Player> p_players);
+	//void Attack2(std::vector<Player> p_players);
 private:		
 	std::string m_username;
 	std::string m_password;
-	sf::Texture m_texture;
 	uint8_t m_keySet;
 	bool m_isDead;
 
 	sf::Vector2f m_velocity;
+	std::vector<Animation> m_animations;
 };
 
 #endif
