6f8446215e7f67fd96a87cd99f51c1db77970157
KevinDuffy <d00144777@student.dkit.ie>
2016-03-29 17:35:44 +0100

    Added Updating HUD text

--- ShadowGameLocal/main.cpp
+++ ShadowGameLocal/main.cpp
@@ -8,9 +8,18 @@
 #include <iostream>
 #include "SFML/Audio.hpp"
 
+//Music
 sf::Music music;
+//HUD
+sf::Font font;
 sf::Sprite hud;
 sf::Texture hudT;
+sf::Text score;
+sf::Text health;
+sf::Text score2;
+sf::Text health2;
+
+//Players
 PlayerDatabase players;
 Player p1;
 Player p2;
@@ -18,6 +27,7 @@ Player p2;
 ShadowFactory shadowFactory;
 TexturedWorld texturedWorld;
 
+//View
 sf::Vector2f p1Cen;
 sf::Vector2f p2Cen;
 sf::View p1View(sf::FloatRect(0, 0, 600, 600));
@@ -31,6 +41,7 @@ sf::Texture t;
 Animation a;
 std::vector<Animation> animations;
 
+//Methods
 void Update();
 void Draw();
 void Load();
@@ -40,7 +51,7 @@ void LoadViewports();
 void LoadAnimation();
 void LoadWorld();
 void LoadPlayers();
-
+void LoadHUD();
 
 sf::RenderWindow window(sf::VideoMode(1260, 700), "SFML works!");
 
@@ -76,6 +87,11 @@ void Update()
 	p1View.setCenter(p1Cen);
 	p2View.setCenter(p2Cen);
 
+	score.setString(players.GetPlayers()[0].GetUsername() + " Score :\t\t" + "0");
+	health.setString(players.GetPlayers()[0].GetUsername() + " Health :\t  " + std::to_string(players.GetPlayers()[0].GetHealth()));
+	
+	score2.setString(players.GetPlayers()[1].GetUsername() + " Score :\t\t" + "0");
+	health2.setString(players.GetPlayers()[1].GetUsername() + " Health :\t  " + std::to_string(players.GetPlayers()[1].GetHealth()));
 }
 void Draw()
 {
@@ -117,7 +133,12 @@ void Draw()
 
 	// Draw hud
 	window.setView(window.getDefaultView());
+	
 	window.draw(hud);
+	window.draw(score);
+	window.draw(health);
+	window.draw(score2);
+	window.draw(health2);
 
 	window.display();
 }
@@ -125,6 +146,7 @@ void Load()
 {
 	LoadMusic();
 	LoadTextures();
+	LoadHUD();
 	LoadWorld();
 	LoadAnimation();
 	LoadViewports();
@@ -144,11 +166,6 @@ void LoadTextures()
 {
 	if (!texturedWorld.load())
 		return;
-
-	if (!hudT.loadFromFile("spritesheet/HUD.png"))
-		return;
-
-	hud.setTexture(hudT);
 }
 void LoadViewports()
 {
@@ -179,4 +196,32 @@ void LoadPlayers()
 	players = PlayerDatabase();
 	players.AddPlayer(p1);
 	players.AddPlayer(p2);
+}
+
+void LoadHUD()
+{
+	font.loadFromFile("font/Chunq.ttf");
+	if (!hudT.loadFromFile("spritesheet/HUD.png"))
+		return;
+	hud.setTexture(hudT);
+
+	score.setFont(font);
+	score.setColor(sf::Color::Black);
+	score.setCharacterSize(20);
+	score.setPosition(sf::Vector2f(950, 10));
+
+	health.setFont(font);
+	health.setColor(sf::Color::Black);
+	health.setCharacterSize(20);
+	health.setPosition(sf::Vector2f(950, 30));
+	
+	score2.setFont(font);
+	score2.setColor(sf::Color::Black);
+	score2.setCharacterSize(20);
+	score2.setPosition(sf::Vector2f(100, 10));
+
+	health2.setFont(font);
+	health2.setColor(sf::Color::Black);
+	health2.setCharacterSize(20);
+	health2.setPosition(sf::Vector2f(100, 30));
 }
